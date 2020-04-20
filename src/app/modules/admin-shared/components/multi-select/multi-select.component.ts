5f46e22fc3db03ce5bc8d059635fa9f19f3e6b8d
srikanth <srikanth.g@pacewisdom.com>
2020-04-20 14:30:11 +0530

    removed code

--- src/app/modules/admin-shared/components/multi-select/multi-select.component.ts
+++ src/app/modules/admin-shared/components/multi-select/multi-select.component.ts
@@ -1,10 +1,8 @@
-// import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormControlName } from "@angular/forms";
+import { FormGroup, FormControl } from "@angular/forms";
 import { FieldConfig } from "../../field.interface";
-import { ReplaySubject, Subject } from 'rxjs';
-import { Component, ElementRef, ViewChild, OnInit, Input, QueryList, ViewChildren } from '@angular/core';
-import { MatAutocompleteSelectedEvent, MatAutocompleteTrigger, MatChipInputEvent } from '@angular/material';
-import { take, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { Component,  OnInit} from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-multi-select',
@@ -21,7 +19,7 @@ export class MultiSelectComponent implements OnInit {
   ngOnInit() {
     this.finaldata = this.field.options;
     this.roleMultiFilterCtrl.valueChanges
-      .pipe(takeUntil(this._onDestroy))
+      .pipe()
       .subscribe(() => {
         this.finaldata = this.filterRolesMulti();
 
@@ -33,9 +31,6 @@ export class MultiSelectComponent implements OnInit {
     return this.field.options.filter(option => option.label.toLowerCase().includes(filterValue));
 
   }
-
-  protected _onDestroy = new Subject<void>();
-
 
 }
 
