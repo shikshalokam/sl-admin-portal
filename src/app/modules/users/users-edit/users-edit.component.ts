1c71216874493b20cedf6c0622c2578abed53b00
srikanth <srikanth.g@pacewisdom.com>
2020-04-21 15:37:34 +0530

    edit-user

--- src/app/modules/users/users-edit/users-edit.component.ts
+++ src/app/modules/users/users-edit/users-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonServiceService } from '../../admin-core/services/common-service.service';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-users-edit',
@@ -9,11 +10,19 @@ import { CommonServiceService } from '../../admin-core/services/common-service.s
 export class UsersEditComponent implements OnInit {
   editUserDetails: any;
   panelOpenState: boolean = false;
-  constructor(private commonServiceService: CommonServiceService) { }
+  constructor(private commonServiceService: CommonServiceService,
+    private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.editUserDetails = this.commonServiceService.showEditUserDetails();
     console.log(' this.editUserDetails', this.editUserDetails);
+  }
+
+  reset() {
+    this._snackBar.open('Comming soon', 'Dismiss', {
+      duration: 1000,
+      verticalPosition: 'top'
+    });
   }
 
 }
